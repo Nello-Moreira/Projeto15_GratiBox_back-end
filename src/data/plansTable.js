c7b61125a97b7c71e434b184a5f1d458b9b56572
Bruna Topan <brutopanb@gmail.com>
2021-11-24 18:09:28 -0300

    Preparando review

--- src/data/plansTable.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable implicit-arrow-linebreak */
-import { dbConnection } from './connection.js';
-
-const searchPlan = (planType) =>
-	dbConnection.query('SELECT * FROM plans WHERE type = $1;', [planType]);
-
-export default searchPlan;
